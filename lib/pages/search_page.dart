0bbf3a1e89ffbed0f07e72816bcf8d7fdbe778cf
William Kukah <williamkukah@outlook.com>
2024-01-12 10:34:46 +0000

    Update search_page.dart

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -5,7 +5,7 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text(
         'Search',
         style: TextStyle(fontSize: 50),
