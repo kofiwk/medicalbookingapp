8be4cdadff224d9139930b7964dd6c9153045d25
William Kukah <williamkukah@outlook.com>
2024-01-12 15:37:40 +0000

    Update search_page.dart

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,15 +1,82 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+
+  late String _greeting;
+
+  void setGreeting() {
+    int hour = DateTime.now().hour;
+    String greeting;
+    if (hour >= 6 && hour < 12) {
+      greeting = "Good Morning";
+    } else if (hour >= 12 && hour < 18) {
+      greeting = "Good Afternoon";
+    } else {
+      greeting = "Good Evening";
+    }
+    setState(() {
+      _greeting = greeting;
+    });
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Search',
-        style: TextStyle(fontSize: 50),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //Greeting
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset('assets/images/Ellipse 3.png',
+                          width: 61, height: 61),
+                      const SizedBox(width: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            _greeting,
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey[400],
+                                fontWeight: FontWeight.bold,
+                                height: 0,
+                                letterSpacing: -0.30),
+                          ),
+                          const Text(
+                            'William Kukah',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.w600,
+                              height: 0,
+                              letterSpacing: -0.30,
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
+
