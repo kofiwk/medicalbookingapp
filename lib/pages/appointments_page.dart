1dca1f3c818e1efc654beee7db18f02d9c270996
William Kukah <williamkukah@outlook.com>
2024-01-12 10:35:14 +0000

    Update appointments_page.dart

--- lib/pages/appointments_page.dart
+++ lib/pages/appointments_page.dart
@@ -5,7 +5,7 @@ class AppointmentsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text(
         'Appointments',
         style: TextStyle(fontSize: 50),
