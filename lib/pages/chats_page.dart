bf22068a94dc3f4eeab871b87068d44b082e1bfd
William Kukah <williamkukah@outlook.com>
2024-01-15 16:01:56 +0000

    Update chats_page.dart

--- lib/pages/chats_page.dart
+++ lib/pages/chats_page.dart
@@ -5,22 +5,42 @@ class ChatsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(25.0),
+          padding: const EdgeInsets.all(25.0),
           child: Column(
             children: [
-              Text(
-                'Chats',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 28,
-                  fontWeight: FontWeight.w600,
-                  height: 0,
-                  letterSpacing: -0.30,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Chats',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                      height: 0,
+                      letterSpacing: -0.30,
+                    ),
+                  ),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: const BoxDecoration(),
+                    child: const Stack(
+                      children: [
+                        Image(
+                          image: AssetImage(
+                            'assets/images/menu.png'
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ],
           ),
