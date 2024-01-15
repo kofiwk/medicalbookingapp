90b405dfd751f61d2536098c7359cbf3d1e243d6
William Kukah <williamkukah@outlook.com>
2024-01-15 16:02:34 +0000

    Update explore_page.dart

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -10,22 +10,41 @@ class ExplorePage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(25.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text(
-                'Explore',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 28,
-                  fontWeight: FontWeight.w600,
-                  height: 0,
-                  letterSpacing: -0.30,
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Explore',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w600,
+                      height: 0,
+                      letterSpacing: -0.30,
+                    ),
+                  ),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: const BoxDecoration(),
+                    child: const Stack(
+                      children: [
+                        Image(
+                          image: AssetImage(
+                            'assets/images/menu.png'
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ],
           ),
-        )
+        ),
       ),
     );
   }
