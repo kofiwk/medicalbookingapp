12cb84f247577a5eed7ac4e25d5f6ab8419e610a
William Kukah <williamkukah@outlook.com>
2024-01-18 23:35:46 +0000

    Update appointments_page.dart

--- lib/pages/appointments_page.dart
+++ lib/pages/appointments_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AppointmentsPage extends StatelessWidget {
+class AppointmentsPage extends StatefulWidget {
   const AppointmentsPage({super.key});
 
+  @override
+  State<AppointmentsPage> createState() => _AppointmentsPageState();
+}
+
+class _AppointmentsPageState extends State<AppointmentsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,19 +33,400 @@ class AppointmentsPage extends StatelessWidget {
                   Container(
                     width: 24,
                     height: 24,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: const BoxDecoration(),
-                    child: const Stack(
-                      children: [
-                        Image(
-                          image: AssetImage(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
                           'assets/images/calendar_month.png'
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 30),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    width: 455,
+                    height: 27,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Upcoming',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: 'Urbanist',
+                            fontWeight: FontWeight.w600,
+                            height: 0,
+                            letterSpacing: -0.30,
+                          ),
+                        ),
+                        SizedBox(width: 13),
+                        Text(
+                          'Pending',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                            fontFamily: 'Urbanist',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                            letterSpacing: -0.30,
+                          ),
+                        ),
+                        SizedBox(width: 13),
+                        Text(
+                          'Past',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                            fontFamily: 'Urbanist',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                            letterSpacing: -0.30,
+                          ),
+                        ),
+                        SizedBox(width: 13),
+                        Text(
+                          'Cancelled',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                            fontFamily: 'Urbanist',
+                            fontWeight: FontWeight.w500,
+                            height: 0,
+                            letterSpacing: -0.30,
                           ),
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Container(
+                        width: 110,
+                        height: 3,
+                        decoration: ShapeDecoration(
+                          color: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12)
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 281,
+                        height: 3,
+                        decoration: ShapeDecoration(
+                          color: Colors.grey,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
+              ),
+
+              // Search Box
+              const SizedBox(height: 30),
+              Container(
+                padding:
+                  const EdgeInsets.symmetric(horizontal: 14, vertical: 10),
+                width: 384,
+                height: 52,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    side: const BorderSide(
+                      width: 1,
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 22,
+                          height: 22,
+                          clipBehavior: Clip.antiAlias,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                'assets/images/search.png'
+                              ),
+                            )
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        const Text(
+                          'Search by name or role',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18,
+                            fontFamily: 'Urbanist',
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: -0.30,
+                            height: 0,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(width: 10),
+                    Container(
+                      width: 24,
+                      height: 24,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(image: 
+                          AssetImage(
+                            'assets/images/filter.png'
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // 1st Appointment
+              const SizedBox(height: 40),
+              Container(
+                width: 384,
+                height: 158,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 81,
+                      height: 80,
+                      decoration: ShapeDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage(
+                            'assets/images/tima.png'
+                          ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 14),
+                    Container(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text.rich(
+                                        TextSpan(
+                                          children: [
+                                            TextSpan(
+                                              text: 'Appointment with',
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 17,
+                                                fontWeight: FontWeight.w400,
+                                                height: 0,
+                                                letterSpacing: -0.30,
+                                              )
+                                            ),
+                                            TextSpan(
+                                              text: ' Dr. Cynthia Chisom',
+                                              style: TextStyle(
+                                                color: Colors.blue,
+                                                fontSize: 17,
+                                                fontWeight: FontWeight.w700,
+                                                height: 0,
+                                                letterSpacing: -0.30,
+                                              )
+                                            )
+                                          ]
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                      const SizedBox(height: 16),
+                                      Container(
+                                        child: Row(
+                                          children: [
+                                            Container(
+                                              child: Row(
+                                                children: [
+                                                  Container(
+                                                    width: 22,
+                                                    height: 22,
+                                                    decoration: BoxDecoration(
+                                                      image: DecorationImage(
+                                                        image: AssetImage(
+                                                          'assets/images/calendar.png'
+                                                        )
+                                                        )
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+
+              // 2nd Appointment 
+              const SizedBox(height: 28),
+              Container(
+                width: 384,
+                height: 158,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 81,
+                      height: 80,
+                      decoration: ShapeDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage(
+                            'assets/images/josephine.png'
+                          ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 14),
+                    Container(
+                      width: 289,
+                      height: 96,
+                      child: const Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Appointment with ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                                letterSpacing: -0.30,
+                              )
+                            ),
+                            TextSpan(
+                              text: 'Dr. Josephine Jane',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                                letterSpacing: -0.30,
+                              )
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                  ],
+                ),
+              ),
+
+              // 3rd Appointment
+              const SizedBox(height: 28),
+              Container(
+                width: 384,
+                height: 158,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 81,
+                      height: 80,
+                      decoration: ShapeDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage(
+                            'assets/images/kelvin.png'
+                          ),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 14),
+                    Container(
+                      width: 289,
+                      height: 96,
+                      child: const Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Appointment with ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                                letterSpacing: -0.30,
+                              )
+                            ),
+                            TextSpan(
+                              text: 'Dr. Kelvin Anthony',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontSize: 17,
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                                letterSpacing: -0.30,
+                              )
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
